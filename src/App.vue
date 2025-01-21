f23bb7812ed482a35ec3e2385d57b7ee876eecc8
winstoncda <olivier.pro.cda@gmail.com>
2025-01-21 07:52:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="app-container">
+    <h1>Hello World !</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./assets/base.scss" as *;
+
+.app-container {
+  min-height: 100vh;
+}
+</style>
